9284dbe3f4c34ae529aa0180d4f74d42eed46405
abouhid <alexandre.bouhid@engenharia.ufjf.br>
2020-09-28 13:07:12 -0300

    add bulma layout

--- Gemfile
+++ Gemfile
@@ -54,3 +54,5 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem "twitter", "~> 7.0"
+
+gem 'bulma-rails', '~> 0.9.0'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :entries
+  root to: "entries#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
